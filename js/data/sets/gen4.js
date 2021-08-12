0ed5b3c1c7784e7dbb0238ff2a9ddc66f7f841b0
Jibril212 <88855183+Jibril212@users.noreply.github.com>
2021-08-12 23:19:41 +0200

    Update gen4.js

--- js/data/sets/gen4.js
+++ js/data/sets/gen4.js
@@ -271,7 +271,22 @@ var SETDEX_DPP = {
                 "Rock Tomb",
                 "Mist",
                 "Rollout"
-            ]            
+            ]   
+        }
+    },
+
+    "Chansey": {
+        "Cheryl": {
+            "level": 20,
+            "item": "Sitrus Berry",
+            "nature": "Quiet",
+            "ability": "Serene Grace",
+            "moves": [
+                "Hyper Voice",
+                "Soft-Boiled",
+                "Ice Beam",
+                "Thunderbolt"
+            ]             
         }
     }
 }
